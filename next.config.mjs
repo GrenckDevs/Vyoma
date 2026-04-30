e2ec083f22f22930fc029bbef1fbfc522c73d708
Chandan G S <chandansuresh2003@gmail.com>
2026-04-30 09:31:27 +0530

    Fix: Set basePath and assetPrefix for GitHub Pages subfolder

--- next.config.mjs
+++ next.config.mjs
@@ -5,9 +5,8 @@ const nextConfig = {
     unoptimized: true,
   },
   // If you are deploying to https://<username>.github.io/<repository-name>/
-  // uncomment the lines below and replace <repository-name> with your repo name
-  // basePath: '/Vyoma',
-  // assetPrefix: '/Vyoma',
+  basePath: '/Vyoma',
+  assetPrefix: '/Vyoma',
 };
 
 export default nextConfig;
